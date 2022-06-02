8cf2746891cc22d3ddef0fc2774ced0b5b4dbbb8
ASIL-E-Hatakeyama <hatakeyama@asil.co.jp>
2022-06-02 15:38:44 +0900

    rustのフォーマッターとかウィンドウにメニューとか追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, Submenu};
+
 fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "閉じる");
+  let close = CustomMenuItem::new("logout".to_string(), "ログアウト");
+  let submenu = Submenu::new("設定", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
   tauri::Builder::default()
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
